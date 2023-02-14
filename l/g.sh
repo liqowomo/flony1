0c58010f9789c2ca3387e96fc83ff36f3dc09f89
liqowomo <123171512+liqowomo@users.noreply.github.com>
2023-02-14 21:27:03 +0000

    🌬️:FLONY1

--- l/g.sh
+++ l/g.sh
@@ -3,7 +3,7 @@ echo "==================="
 echo ""
 echo "git add -all && git commit -m <addurshit>&& git push"
 git add --all 
-git commit -m "🇰🇿:FLONY1"
+git commit -m "🌬️:FLONY1"
 git push --progress
 echo ""
 echo "=========PUSH DONE========="
